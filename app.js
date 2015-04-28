58e9fae479ae78e2c7e9ea59390588bceb50e214
sndsgn <forloine@gmail.com>
2015-04-27 17:56:15 -0700

    Splits out GHI and LA Electricity XHR requests from Weather API request.

--- app.js
+++ app.js
@@ -1,45 +1,17 @@
 //Current Weather API is giving weather per day or per month per year
 //Current Electricty APIs for SF, LA and CHI are per year
-//
-//1st Need to know the number of hours of sunlight for a city over an entire year. Actually let's start with a day and the known energy needs for a specific zip.
-//2nd Need to know the electricity or energy total usage for that city for an entire year
-//3rd Need to calculate how many solar panels would be needed to meet the power needs for the hours of sunlight
-//3rd Need to know the sqft of that neighborhood to determine if the solar panels would fit
-//
-//1. Get the sunlight time per month for all of the zip codes in a city. Or alternatively get it for one geolocation.
-//2. 
-//
-//
-//
-
-//
-//
 //AJAX request
-//
 
 //Inspired by http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-//http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
 function thousandCommaSeparator(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
-//Inspired by http://eloquentjavascript.net/17_http.html
-var laZipElectricity = new XMLHttpRequest();
-laZipElectricity.open('GET', 'https://data.lacity.org/api/views/rijp-9dwj/rows.json', true);
-laZipElectricity.send(null);
 
 var zipWeather = new XMLHttpRequest();
 zipWeather.open('GET', '//api.worldweatheronline.com/premium/v1/past-weather.ashx?q=Los+Angeles&format=json&date=2009-12-23&key=5362d7ee160aa329b22c87b89aae8', true);
 zipWeather.send(null);
 
-var ghi = new XMLHttpRequest();
-ghi.open('GET', 'http://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=DPx3KUp9krBKyHIiDqd1axWqETim9pYy3BwT6f5z&address=Los+Angeles', true);
-ghi.send(null);
-
-
 zipWeather.addEventListener('load', function() {
-
-
 //Hours Sun Visible ***NOT YET SHOWING SUN VISIBLE TIME FOR TODAY() AND CITY CHOSEN***
   var zipWeatherObj = JSON.parse(zipWeather.responseText);
   var zipWeatherDate = zipWeatherObj['data']['weather'][0]['date']; 
@@ -52,9 +24,7 @@ zipWeather.addEventListener('load', function() {
   var sunSetDateHours = sunSetDateTime.getHours(); 
   var sunSetDateMinutes = sunSetDateTime.getMinutes(); 
   var sunVisibleTimeHours = (sunSetDateHours - sunRiseDateHours);
-  console.log(sunVisibleTimeHours, typeof sunVisibleTimeHours); 
   var sunVisibleTimeMinutes = (sunSetDateMinutes - sunRiseDateMinutes);
-  console.log(sunVisibleTimeMinutes, typeof sunVisibleTimeMinutes); 
   var sunVisibleTime = function() { 
     if(sunVisibleTimeMinutes < 0) {
       return ((sunVisibleTimeHours - 1).toString() + ' hours ' + (60 + sunVisibleTimeMinutes).toString() + ' minutes');
@@ -63,7 +33,20 @@ zipWeather.addEventListener('load', function() {
       }
    };
   document.getElementById('sunVisible').innerHTML = sunVisibleTime();
+});
 
+
+//GHI Data for Los Angeles
+var ghi = new XMLHttpRequest();
+ghi.open('GET', 'http://developer.nrel.gov/api/solar/solar_resource/v1.json?api_key=DPx3KUp9krBKyHIiDqd1axWqETim9pYy3BwT6f5z&address=Los+Angeles', true);
+ghi.send(null);
+
+//Inspired by http://eloquentjavascript.net/17_http.html
+var laZipElectricity = new XMLHttpRequest();
+laZipElectricity.open('GET', 'https://data.lacity.org/api/views/rijp-9dwj/rows.json', true);
+laZipElectricity.send(null);
+
+ghi.addEventListener('load',  function() {
 //ghIrradiance data object
   var ghIrradianceObj = JSON.parse(ghi.responseText);
   var ghIrradianceDailyAvg = ghIrradianceObj['outputs']['avg_ghi']['annual'];
@@ -115,20 +98,8 @@ zipWeather.addEventListener('load', function() {
   document.getElementById('cityPopulation').innerHTML = thousandCommaSeparator(laCityPop);
 
 //Contribution per Citizen
-  console.log(solarPanelsCost / laCityPop);
   var perResidentContribution = (solarPanelsCost / laCityPop);
   document.getElementById('perResidentContribution').innerHTML = '$' + thousandCommaSeparator(perResidentContribution.toFixed(2)); 
 });
 
-
-
-
-
-//Calculation Ideas: Grape Solar has a 390w panel with 15.21% efficiency and is made in the US.
-//http://www.livescience.com/41747-best-solar-panels.html
-//
-
-
-
-    
 
