31922883e527c0327eb5197a3d76b6781593daf7
sndsgn <forloine@gmail.com>
2015-05-02 15:39:10 -0700

    Adds additional CO2 stats and percent format function.

--- app.js
+++ app.js
@@ -27,9 +27,12 @@ ghi.addEventListener('load',  function() {
     var ghIrradianceDailyAvg = ghIrradianceObj['outputs']['avg_ghi']['annual'];
     document.getElementById('ghIrradiance').innerHTML = ghIrradianceDailyAvg;
 
+    //Days in a year
+    var daysInYear = 365.24;
+
     //Los Angeles Electricity for 2012-2013 broken to into daily use
     var laElectricityObj = JSON.parse(laElectricityAnnual.responseText);
-    var laElectricityDailyAvg = Math.round((laElectricityObj.meta.view.columns[15].cachedContents.sum * 1000000) / 365.24);
+    var laElectricityDailyAvg = Math.round((laElectricityObj.meta.view.columns[15].cachedContents.sum * 1000000) / daysInYear);
     document.getElementById('laElectricityDailyUsage').innerHTML = thousandCommaSeparator(laElectricityDailyAvg);
 
     //Solar Panels Needed based on Grape Solar Panel 390 watt
@@ -52,17 +55,37 @@ ghi.addEventListener('load',  function() {
     var laAreaSqft = Math.round(469*27889333.33333);
     document.getElementById('laSqft').innerHTML = thousandCommaSeparator(laAreaSqft);
 
+    //Percent format function
+    var percentFormat = function(num) {
+      return ((num * 100).toFixed(2)) + '%';
+    };
+
     //Percent of city area covered in solar panels
-    var laAreaPercentForSolar = ((laAreaForSolarSqftNum / laAreaSqft) * 100).toFixed(2); 
-    document.getElementById('laAreaPercentForSolar').innerHTML = laAreaPercentForSolar + '%';
+    var laAreaPercentForSolar = percentFormat(laAreaForSolarSqftNum / laAreaSqft); 
+    document.getElementById('laAreaPercentForSolar').innerHTML = laAreaPercentForSolar;
+
+    //co2 production reduced per kWh
+    //Source: http://www.eia.gov/tools/faqs/faq.cfm?id=74&t=11
+    var lbsCo2PerKwh = 2.07;
+    var tonsCo2PerKwh = lbsCo2PerKwh / 2000;
+    var tonsCo2Reduced =  (((laElectricityDailyAvg * daysInYear) * tonsCo2PerKwh).toFixed(0));
+    document.getElementById('co2Reduced').innerHTML = thousandCommaSeparator(tonsCo2Reduced);
+
+    //CO2 reduction as percent of California emmissions
+    //Source: http://www.arb.ca.gov/cc/inventory/data/misc/ghg_inventory_trends_00-12_2014-05-13.pdf
+    //Data from 2012 which since there is a small reduction every year this number is probably higher than today's annual emmissions
+    var californiaCo2 = 458.7 * 1000000;
+    var percentCo2OfCalifornia = tonsCo2Reduced / californiaCo2; 
+    document.getElementById('percentCo2California').innerHTML = percentFormat(percentCo2OfCalifornia);  
+
 
    //Gallons of water saved per year based on stats here for nuclear energy being 600 gallons per mWh http://cleantechnica.com/2014/03/22/solar-power-water-use-infographic/
-    var waterSavedPerKwh = (600/1000) * laElectricityDailyAvg;
+    var waterSavedPerKwh = (600/1000) * (laElectricityDailyAvg * daysInYear);
     document.getElementById('waterSaved').innerHTML = thousandCommaSeparator(Math.round(waterSavedPerKwh));
 
     //Cost of solar panels needed to make the city sustainable
     //Source for price per Grape Solar 390 watt panel: http://www.freecleansolar.com/390W-solar-panel-Grape-GS-S-390-TS-mono-p/gs-s-390-ts.htm?gclid=Cj0KEQjwyIyqBRD4janGs5e67IsBEiQAoF8DGtxLzucEC_zw3ED8ARPd3pIyd8wjR5V0w9Cj8bWJBq4aAtwI8P8HAQ
-    var solarPanelsCost = 585 * numSolarPanels;
+    var solarPanelsCost = grapeSolarPanelPrice * numSolarPanels;
     document.getElementById('solarPanelTotalCost').innerHTML = '$' + thousandCommaSeparator(solarPanelsCost);
 
     //Los Angeles average cost per kWh http://www.bls.gov/regions/west/news-release/averageenergyprices_losangeles.htm ***API DOES EXIST - CHECK IT OUT***
@@ -82,12 +105,12 @@ ghi.addEventListener('load',  function() {
       };
 
       //Return on investment - Number of years before you pay off Solar Panels
-      var roiYearsPayOffSolar = (solarPanelsCost / (laElectricityCostDaily * 365.24) / (1 - solarDepreciationAvg(1)));
+      var roiYearsPayOffSolar = (solarPanelsCost / (laElectricityCostDaily * daysInYear) / (1 - solarDepreciationAvg(1)));
       document.getElementById('roiDays').innerHTML = (roiYearsPayOffSolar).toFixed(2);
 
       //Twenty year savings including degradation of panel output
       //Source: http://www.engineering.com/ElectronicsDesign/ElectronicsDesignArticles/ArticleID/7475/What-Is-the-Lifespan-of-a-Solar-Panel.aspx
-      var twentyNetYearSavings = ((((laElectricityCostDaily * 20) * (1 - solarDepreciationAvg(20))) * 365.24) - solarPanelsCost);
+      var twentyNetYearSavings = ((((laElectricityCostDaily * 20) * (1 - solarDepreciationAvg(20))) * daysInYear) - solarPanelsCost);
       document.getElementById('twentyNetYearSavings').innerHTML = '$' + thousandCommaSeparator(Math.round(twentyNetYearSavings));
 
     //Population of City
